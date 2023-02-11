0810a9b5930f113a393abd4a6f81fb2c36deebe2
Emerson Pita <emersonpita2016@gmail.com>
2023-02-11 20:39:09 -0300

    Adição da última sessão e formulário de contato

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Animações de Entrada. (ScrollReveal)
+// Animações de Entrada. (ScrollReveal)
 window.sr = ScrollReveal({reset: true});
 
 sr.reveal("header", {duration: 1500});
@@ -7,9 +7,12 @@ sr.reveal(".text-container-section2", {duration: 1500});
 sr.reveal(".img-container-section2", {duration: 1500});
 sr.reveal(".section-2-5", {duration: 1500});
 sr.reveal(".principal-text-section3", {duration: 1500});
-sr.reveal(".img-container-section3", {distance: "300px", origin: "bottom", duration: 1500});
+sr.reveal(".img-container-section3", {distance: "100px", origin: "bottom", duration: 1500});
 sr.reveal(".text-container-section3", {distance: "300px", origin: "left", duration: 1500});
 sr.reveal(".text2-container-section3", {distance: "300px", origin: "right", duration: 1500});
+sr.reveal(".principal-text-section4", {duration: 1500});
+sr.reveal(".card", {distance: "300px", origin: "bottom", duration: 1500});
+sr.reveal(".form-container", {duration: 1500});
 
 // Armazena os links do menu de navegação
 const navigationLinks = document.querySelectorAll(".item-menu");
@@ -36,3 +39,45 @@ window.addEventListener('scroll', function() {
   }
 });
 
+// Armazena o formulário
+const form = document.getElementById("form");
+  // Adiciona o evento de submit no formulário
+  form.addEventListener("submit", function (event) {
+    // Remove o evento padrão de recarregamento da página
+    event.preventDefault();
+    verifyForm()
+    resetForm()
+
+  });
+
+  // Verifica se os inputs estão preenchidos
+  function verifyForm() {
+    const form = document.getElementById("form");
+    const field1 = document.getElementById("name");
+    const field2 = document.getElementById("email");
+    const field3 = document.getElementById("message");
+
+    if (field1.value && field2.value && field3.value) {
+      field1.classList.add("verified");
+      field2.classList.add("verified");
+      field3.classList.add("verified");
+    } else {
+      field1.classList.remove("verified");
+      field2.classList.remove("verified");
+      field3.classList.remove("verified");
+    }
+  }
+
+  // Reseta os campos do formulário após 2 segundos que ele é enviado
+  function resetForm() {
+    const field1 = document.getElementById("name");
+    const field2 = document.getElementById("email");
+    const field3 = document.getElementById("message");
+    setTimeout (function () {
+        form.reset()
+        field1.classList.remove("verified");
+        field2.classList.remove("verified");
+        field3.classList.remove("verified");
+
+    }, 2000);
+  }
